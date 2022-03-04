3f88c37c99306c6767a54d8b14bde59eb0a182a2
Pol <pacadi92@gmail.com>
2022-03-04 02:02:08 +0100

    asset folder
    added an asset folder

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import pandas as pd
 
 screen = tl.Screen()
 screen.title("U.S.A. States Game")
-image = "blank_states_img.gif"
+image = "assets/blank_states_img.gif"
 screen.addshape(image)
 tl.shape(image)
-data = pd.read_csv("50_states.csv")
+data = pd.read_csv("assets/50_states.csv")
 all_states = data.state.to_list()
 guessed_states = []
 
@@ -14,9 +14,10 @@ while len(guessed_states) < 50:
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
                                     prompt="What's another state's name?").title()
     if answer_state == "Exit":
-        missing_states = [state for state in all_states if state not in guessed_states]
+        missing_states = [
+            state for state in all_states if state not in guessed_states]
         new_data = pd.DataFrame(missing_states)
-        new_data.to_csv("states_to_learn.csv")
+        new_data.to_csv("assets/states_to_learn.csv")
         break
     if answer_state in all_states:
         guessed_states.append(answer_state)
